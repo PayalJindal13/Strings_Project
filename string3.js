14d7525e334b642b4c1eff3f8791a84cf200e018
Payal Jindal <payal.jindal.19.1@mountblue.tech>
2022-03-11 08:58:19 +0530

    Month of date

---
+++ string3.js
@@ -0,0 +1,18 @@
+module.exports = function monthOfDate(dateString){
+    // return month for date
+    if (dateString){
+        
+        // Splitting the date components
+        let dateSplit = dateString.split('/');
+        let dd = Number(dateSplit[0]);
+        let mm = Number(dateSplit[1]);
+        let yy = dateSplit[2];
+
+        //check if its a valid date and return month
+        if(!isNaN(Date.parse(yy.toString()+'-'+mm.toString()+'-'+dd.toString()))) return mm;
+        
+     }
+
+    // return undefined for undefined, null, empty string or invalid date
+    return undefined;
+}
---
+++ test/testString3.js
@@ -0,0 +1,6 @@
+let monthFromDate = require('../string3.js');
+let dateInput = '01/12/2021';
+
+// Expects month for valid date and undefine for null,undefined or invalid date
+console.log(monthFromDate(dateInput));
+
