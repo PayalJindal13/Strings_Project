bc2a303ea5d3bcf24f0cae50cd7a1a1150b68ced
Payal Jindal <payal.jindal.19.1@mountblue.tech>
2022-03-11 11:27:33 +0530

    Testcases added to ipv4 to array file

--- test/testString2.js
+++ test/testString2.js
@@ -1,6 +1,11 @@
 let addressToArray = require('../string2.js');
-let ipAdd = '254.27.56.61';
 
-// Expects array of components for valid address and empty array for null,undefined or invalid
-console.log(addressToArray(ipAdd));
+// Testcases
+console.log(addressToArray('192.89.45.12'));    // expects output [192, 89, 45, 12]
+console.log(addressToArray('355.23.67.100'));   // expects empty string as invalid address is given
+console.log(addressToArray('123.234.56.78.80')); // expects empty string as invalid address is given
+console.log(addressToArray());                   // expects empty string
+console.log(addressToArray(''));                 // expects empty string
+console.log(addressToArray(null));                // expects empty string
+console.log(addressToArray(undefined));             // expects empty string
 
