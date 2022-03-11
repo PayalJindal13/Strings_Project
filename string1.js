5fb9f7e1ea72d9842ec44d5abf219a0722a71cd3
Payal Jindal <payal.jindal.19.1@mountblue.tech>
2022-03-11 07:24:42 +0530

    Convert string to number

---
+++ string1.js
@@ -0,0 +1,25 @@
+module.exports = function stringToNumber(stringOfNum){
+
+    if (stringOfNum){
+
+        // Omitting $ and , from the string
+        let stringOfNumWithout$ = stringOfNum.replace('$','');
+        let stringOfNumOnly = stringOfNumWithout$.replaceAll(',','');
+
+        // Converting a string of negative number into a number
+        if (stringOfNumOnly[0] === '-'){
+            let numValue = Number(stringOfNumOnly.slice(1));
+            if (isNaN(numValue)) return 0;
+            return -numValue;
+        }
+
+        // Converting a string of positive number into number 
+        else{
+            let numValue = Number(stringOfNumOnly.slice(0));
+            if (isNaN(numValue)) return 0;
+            return numValue;
+        }
+    }
+    // Returning undefined if string is null,empty or undefined
+    return undefined;
+}
---
+++ test/testString1.js
@@ -0,0 +1,4 @@
+let stringToNum = require('../string1.js');
+let numberAsString = '-$125.909';
+// Expects a number if valid number string is passed, 0 if its not a valid number, and undefined otherwise
+console.log(stringToNum(numberAsString));
