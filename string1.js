7dc349532ce7cc4888eb14abf6d323fa5652ed5b
Payal Jindal <payal.jindal.19.1@mountblue.tech>
2022-03-12 16:49:30 +0530

    String to number file updated

--- string1.js
+++ string1.js
@@ -2,24 +2,65 @@ module.exports = function stringToNumber(stringOfNum){
 
     if (stringOfNum){
 
-        // Omitting $ and , from the string
-        let stringOfNumWithout$ = stringOfNum.replace('$','');
-        let stringOfNumOnly = stringOfNumWithout$.replaceAll(',','');
+        // Omitting $ from the string
+        let str = stringOfNum.replace('$','');
+        let stringOfNumOnly = '';
+        let numValue;
 
-        // Converting a string of negative number into a number
-        if (stringOfNumOnly[0] === '-'){
-            let numValue = Number(stringOfNumOnly.slice(1));
+        // Without decimals numbers
+        if (str.indexOf('.') < 0){
+            if(str[0] !== '-') stringOfNumOnly = stringNumPostive(str);
+            if(str[0] === '-') stringOfNumOnly = stringNumNegative(str);
+            numValue = Number(stringOfNumOnly);
             if (isNaN(numValue)) return 0;
-            return -numValue;
+            return numValue;
         }
 
-        // Converting a string of positive number into number 
-        else{
-            let numValue = Number(stringOfNumOnly.slice(0));
+        // With decimal numbers
+        else if (str.indexOf('.') > 0){
+            let str1 = str.substring(0,str.indexOf('.'));
+            if(str1[0] !== '-') str1 = stringNumPostive(str1);
+            if(str1[0] === '-') str1 = stringNumNegative(str1);
+            let str2 = str.substring(str.indexOf('.')+1);
+            numAfterDecimal = Number(str2);
+            if(isNaN(numAfterDecimal)) return 0;
+            stringOfNumOnly = str1 + '.' + str2;
+            numValue = Number(stringOfNumOnly);
             if (isNaN(numValue)) return 0;
             return numValue;
         }
     }
     // Returning undefined if string is null,empty or undefined
     return 0;
+}
+
+
+function stringNumPostive(str){
+    let stringOfNumOnly;
+    if (str.length > 4){
+        for (let i = 1; i <= str.length -4; i += 4){
+            if(str[i] === ','){
+                stringOfNumOnly = str.substring(0,i) + str.substring(i+1);
+            }
+        }
+    }
+    else{
+        stringOfNumOnly = str;
+    }
+    return stringOfNumOnly;
+}
+
+function stringNumNegative(str){
+    let stringOfNumOnly = str;
+    if (str.length > 5){
+        for (let i = 2; i <= str.length -4; i += 4){
+            if(str[i] === ','){
+                stringOfNumOnly = str.substring(0,i) + str.substring(i+1);
+            }
+        }
+    }
+    else{
+        stringOfNumOnly = str;
+    }
+    return stringOfNumOnly;
 }
--- test/testString1.js
+++ test/testString1.js
@@ -1,8 +1,12 @@
 let stringToNum = require('../string1.js');
-console.log(stringToNum('$340'));       // expects output 340
+console.log(stringToNum('$200'));           //expects output 200
+console.log(stringToNum('$340.99'));       // expects output 340.99
+console.log(stringToNum('$5,640'));       // expects output 5640
+console.log(stringToNum('-$567'));       // expects output 567
 console.log(stringToNum('-$120.45'));   // expects output -120.45
+console.log(stringToNum('-$1,200.85')); // expects output -1200.85
 console.log(stringToNum('$120y89'));    // expects output 0 dur to invalid number
-console.log(stringToNum());             // expects output 0
+console.log(stringToNum('$10,,,,00'));   // expects output 0
 console.log(stringToNum(undefined));    // expects output 0
 console.log(stringToNum(null));         // expects output 0
 
