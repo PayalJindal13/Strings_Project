ea47dceb788cd0ad05e5c57f6ff2a80e9cb6a3db
Payal Jindal <payal.jindal.19.1@mountblue.tech>
2022-03-12 12:56:27 +0530

    String file 4 changed

--- string4.js
+++ string4.js
@@ -2,23 +2,27 @@ module.exports = function titleCaseName(nameObj){
 
     // returning titledcase name for a defined name object
     if (nameObj && Object.keys(nameObj).length){ // check if it is not null, undefined or empty object
-
+        let titledName = '';
         // first name in lowercase 
-        let firstName = nameObj['first_name'].toLowerCase();
+        let firstName;
+        if (nameObj['first_name']) {firstName = nameObj['first_name'].toLowerCase();
+        titledName += firstName.charAt(0).toUpperCase() + firstName.slice(1) + ' ';
+        }
+        
 
         //middle name in lowercase
         let middleName;
-        if (nameObj['middle_name']) {middleName = nameObj['middle_name'].toLowerCase()};
+        if (nameObj['middle_name']) {middleName = nameObj['middle_name'].toLowerCase();
+        titledName += middleName.charAt(0).toUpperCase() + middleName.slice(1) + ' ';
+        }
 
         //last name in lowercase
-        let lastName = nameObj['last_name'].toLowerCase();
-
-        // converting each names in titlecase and combining into one string
-        let titledName = firstName.charAt(0).toUpperCase() + firstName.slice(1) + ' ';
-        if (middleName){ titledName += middleName.charAt(0).toUpperCase() + middleName.slice(1) + ' '}
+        let lastName;
+        if (nameObj['last_name']) {lastName = nameObj['last_name'].toLowerCase();
         titledName += lastName.charAt(0).toUpperCase() + lastName.slice(1);
+        }
         return titledName;
-    }
+        }
 
     //Returning empty string for undefined, null cases and empty object input
     return '';
--- test/testString4.js
+++ test/testString4.js
@@ -1,9 +1,12 @@
 let titleCasedName = require('../string4.js');
 let nameObject = {'first_name' : 'Payal', 'middle_name' : 'Jindal' , 'last_name' : 'Gupta'};
-let nameObject1 = {'first_name' : 'Payal', 'last_name' : 'Gupta'};
 // Testcases
 console.log(titleCasedName(nameObject)); // expects output Payal Jindal Gupta
-console.log(titleCasedName(nameObject1)); // expects output Payal Gupta
+console.log(titleCasedName({'first_name': 'Payal', 'last_name' : 'Gupta'})); //expects output Payal Gupta
+console.log(titleCasedName({'first_name': 'Payal', 'middle_name' : 'Jindal'})); //expects output Payal Jindal
+console.log(titleCasedName({'first_name' : 'Gupta'})); //expects output Payal
+console.log(titleCasedName({'middle_name' : 'Jindal'})); //expects output Jindal
+console.log(titleCasedName({'last_name' : 'Gupta'})); //expects output Gupta
 console.log(titleCasedName({})); // expects empty string as ouput
 console.log(titleCasedName()); // expects empty string as output
 console.log(titleCasedName(null)); // expects empty string as ouput
